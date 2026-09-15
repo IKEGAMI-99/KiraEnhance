d4bb69ab664d162f46bc24209f8e5013cc5f74e7
IKEGAMI-99 <hato.jp@gmail.com>
2026-09-15 23:08:26 +0900

    feat: add device capability model

---
+++ app/src/main/java/com/ikegami99/kiraenhance/device/DeviceCapabilities.kt
@@ -0,0 +1,28 @@
+package com.ikegami99.kiraenhance.device
+
+enum class SupportTier {
+    RECOMMENDED,
+    SUPPORTED,
+    NOT_RECOMMENDED,
+    UNSUPPORTED,
+}
+
+data class DeviceCapabilities(
+    val totalRamMb: Long,
+    val availableStorageMb: Long,
+    val vulkanVersion: String,
+    val deviceName: String,
+    val androidApi: Int,
+    val supportTier: SupportTier,
+)
+
+fun classifyDevice(
+    totalRamMb: Long,
+    hasVulkan12: Boolean,
+    is64Bit: Boolean,
+): SupportTier = when {
+    !is64Bit || !hasVulkan12 -> SupportTier.UNSUPPORTED
+    totalRamMb >= 12_000 -> SupportTier.RECOMMENDED
+    totalRamMb >= 8_000 -> SupportTier.SUPPORTED
+    else -> SupportTier.NOT_RECOMMENDED
+}
